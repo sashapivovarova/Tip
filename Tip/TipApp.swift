88839064d8bc026614d5f29972bc9698b8bc059e
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-17 13:44:19 +0900

    Initial Commit

---
+++ Tip/TipApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipApp.swift
+//  Tip
+//
+//  Created by Anastasia Grey on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct TipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
