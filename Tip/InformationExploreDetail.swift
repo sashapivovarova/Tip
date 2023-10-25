968f122b562e6376183dacd07c35358e09266516
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-25 17:20:03 +0900

    tap view を追加

--- Tip/InformationExploreDetail.swift
+++ Tip/InformationExploreDetail.swift
@@ -9,55 +9,78 @@ import SwiftUI
 
 struct InformationExploreDetail: View {
     
-    @State var selectedPage = 0
-    
     var body: some View {
-        VStack{
-            Image("detail")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 550)
-                .padding(.bottom, 30)
-            
+        ScrollView {
             HStack {
-                VStack(alignment: .leading) {
-                    Text("홈으로 바로 이동하기")
-                        .font(.system(size: 25, weight: .bold))
-                        .padding(.bottom, 3)
-                    Text("홈 화면으로 이동하려면 화면 하단 가장자리를 쓸어올리세요")
+                TabView {
+                    VStack{
+                        Image("detail")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 500)
+                            .padding(.bottom, 30)
+                        
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("홈으로 바로 이동하기")
+                                    .font(.system(size: 25, weight: .bold))
+                                    .padding(.bottom, 3)
+                                Text("홈 화면으로 이동하려면 화면 하단 가장자리를 쓸어올리세요")
+                            }
+                            .padding(.horizontal, 30)
+                            Spacer()
+                        }
+                    }
+                    
+                    VStack{
+                        Image("detail2")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 500)
+                            .padding(.bottom, 30)
+                        
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("앱 전환하기")
+                                    .font(.system(size: 25, weight: .bold))
+                                    .padding(.bottom, 3)
+                                Text("화면을 쓸어올리고 잠시 멈춘 다음, 앱을 탭하여 여세요")
+                            }
+                            .padding(.horizontal, 30)
+                            Spacer()
+                        }
+                    }
+                    
+                    VStack{
+                        Image("detail3")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 500)
+                            .padding(.bottom, 30)
+                        
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("제어 센터 열기")
+                                    .font(.system(size: 25, weight: .bold))
+                                    .padding(.bottom, 3)
+                                Text("화면 오른쪽 상단을 쓸어내려 보세요")
+                            }
+                            .padding(.horizontal, 30)
+                            Spacer()
+                        }
+                    }
+                    
                 }
-                .padding(.horizontal, 30)
-                Spacer()
-            }
-
-            Spacer()
-            PageControl(numberOfPages: 7, currentPage: $selectedPage)
-        }
-        .navigationTitle("탐색하기")
-        .toolbar(content: {
-            Button(action: {
-                
-            }, label: {
-                Image(systemName: "bookmark")
-            })
-        })
-    }
-}
-
-struct PageControl: View {
-    
-    var numberOfPages: Int
-    @Binding var currentPage: Int
-    
-    var body: some View {
-        HStack {
-            ForEach(0 ..< numberOfPages) { index in
-                Circle()
-                    .frame(width: 8, height: 8)
-                    .foregroundColor(index == self.currentPage ? .white : .gray)
-                    .onTapGesture(perform: {
-                        self.currentPage = index
+                .navigationTitle("탐색하기")
+                .toolbar(content: {
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "bookmark")
                     })
+                })
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - 110)
+                .tabViewStyle(.page(indexDisplayMode: .always))
             }
         }
     }
